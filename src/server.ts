810ab3ea578837de8be2ce3745ea5a6b34b05015
Anushka <ite001191@athuman.com>
2023-05-19 03:41:30 +0900

    all cors

--- src/server.ts
+++ src/server.ts
@@ -27,7 +27,7 @@ const mongoDBstore = new MongoDBStore({
 app.use(
   cors({
     allowedHeaders: ['Content-Type', 'Authorization'],
-    origin: "https://anushkawarapitiya.github.io",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"],
     credentials: true
   })
